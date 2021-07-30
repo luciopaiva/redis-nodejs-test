8247974b30df4ff34f73b77e807754dbf183025d
Lucio Paiva <luciopaiva@gmail.com>
2021-07-30 23:13:31 +0100

    Rollback Lua script changes

--- experiments/sorted-set-of-byte-arrays/store-items.lua
+++ experiments/sorted-set-of-byte-arrays/store-items.lua
@@ -10,25 +10,23 @@ It is important to mention that Redis prevents multi-slot operations even when a
 Reference: https://redis.io/topics/cluster-spec#implemented-subset
 --]]
 
---if (#ARGV == 0 or #ARGV % 2 ~= 1) then
---    return
---end
---
---local count = (#ARGV - 1)  / 2
---local nowInMillis = ARGV[1]
---
---local done = 0
+if (#ARGV == 0 or #ARGV % 2 ~= 1) then
+    return
+end
 
---for i = 1, count do
---    local key = ARGV[i + 1]
---    local value = ARGV[i + count + 1]
---
---    --redis.call("setex", key, 3, value)
---    --redis.call("zadd", "latest-ids", nowInMillis, value)
---
---    done = done + 1
---end
---
---return done
+local count = (#ARGV - 1)  / 2
+local nowInMillis = ARGV[1]
 
-return 123
+local done = 0
+
+for i = 1, count do
+    local key = ARGV[i + 1]
+    local value = ARGV[i + count + 1]
+
+    redis.call("setex", key, 3, value)
+    redis.call("zadd", "latest-ids", nowInMillis, value)
+
+    done = done + 1
+end
+
+return done
